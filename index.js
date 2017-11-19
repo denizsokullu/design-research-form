3cb43992bb26611367271306dd72d024c4406be5
Deniz A. Sokullu <deniz.sokullu@ibm.com>
2017-11-19 18:24:31 -0500

    added the form

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var path = require('path')
+var app = express()
+
+
+app.use(express.static(path.join(__dirname,'./public')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+// app.get(/\babout|lines|people\b/, function (req, res) {
+//     res.sendFile(__dirname + '/public/' + req.path + '.html');
+// });
+
+app.get('*', function (req, res) {
+  res.status(404).send("Status code 404 - The URL you are looking for does not exist");
+});
+
+// Start the server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, function() {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+counter = 0;
+length = 720;
+numOfQuestions = 8;
+$(window).ready(function(){
+  $('a[href^="#"]').click(function(event) {
+      console.log("asd");
+      var target = $($(this).attr('href'))[0];
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: target.offsetTop -96
+      },700);
+      counter ++;
+      if(counter > numOfQuestions){
+        return;
+      }
+      if(counter == numOfQuestions){
+        $(".progress-inner").css("width",(length/numOfQuestions*counter)-2);
+      }
+      else{
+        $(".progress-inner").css("width",length/numOfQuestions*counter);
+      }
+
+  });
+})
+$(window).ready(function(){
+  window.scrollTo(0,0);
+})

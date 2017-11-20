6fa868f8f3aeee6a5360b3149cb9c50456f7cb5a
Deniz A. Sokullu <deniz.sokullu@ibm.com>
2017-11-20 15:46:25 -0500

    added placeholde hero image on the bottom

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,26 @@
 counter = 0;
 length = 720;
 numOfQuestions = 6;
+
+var database = firebase.database();
+var ref = database.ref('submissions');
+var curSub = {};
+
 $(window).ready(function(){
+  window.scrollTo(0,0);
   $('a[href^="#"]').click(function(event) {
-      console.log("asd");
       var target = $($(this).attr('href'))[0];
       event.preventDefault();
       $('html, body').animate({
           scrollTop: target.offsetTop -96
       },700);
       counter ++;
+      //update this to be the actual information
+      curSub[parseInt(counter)] = "Yes";
+      if($(this).attr('href').includes("result")){
+        console.log('submitted!');
+        ref.push(curSub);
+      }
       if(counter > numOfQuestions){
         return;
       }
@@ -21,7 +32,4 @@ $(window).ready(function(){
       }
 
   });
-})
-$(window).ready(function(){
-  window.scrollTo(0,0);
 })
